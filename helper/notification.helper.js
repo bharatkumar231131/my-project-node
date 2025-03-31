13e67dfcfddf756bc58d4fbc323afbfa4698b362
bharatkumar231131 <bharatkumarwd@gmail.com>
2025-03-31 12:01:54 +0530

    update

--- helper/notification.helper.js
+++ helper/notification.helper.js
@@ -1,31 +1,31 @@
-// import sgMail from '@sendgrid/mail';
+// // import sgMail from '@sendgrid/mail';
 
-import nodemailer from "nodemailer";
-import nodemailerSendgrid from "nodemailer-sendgrid";
+// import nodemailer from "nodemailer";
+// import nodemailerSendgrid from "nodemailer-sendgrid";
 
-const transporter = nodemailer.createTransport(
-    nodemailerSendgrid({
-        // apikey:'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'
-    })
-);
+// const transporter = nodemailer.createTransport(
+//     nodemailerSendgrid({
+//         // apikey:'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'
+//     })
+// );
 
-const sendEmail = async (to, subject, text, html) => {
-    try {
-        const mailOptions = {
-            from: process.env.SGMAIL,
-            to,
-            subject,
-            text,
-            html
-        };
+// const sendEmail = async (to, subject, text, html) => {
+//     try {
+//         const mailOptions = {
+//             from: process.env.SGMAIL,
+//             to,
+//             subject,
+//             text,
+//             html
+//         };
 
-        const info = await transporter.sendMail(mailOptions);
-        return info;
-    } catch (error) {
-        console.error("Error sending email:", error);
-        throw error;
-    }
-};
+//         const info = await transporter.sendMail(mailOptions);
+//         return info;
+//     } catch (error) {
+//         console.error("Error sending email:", error);
+//         throw error;
+//     }
+// };
 
-export default sendEmail;
+// export default sendEmail;
 
